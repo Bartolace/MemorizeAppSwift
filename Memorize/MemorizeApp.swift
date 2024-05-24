3223feb457b0bdd49d2ebdab2842d0130ba4071f
gabrielBartolace <gabrielll.freitas@hotmail.com>
2024-05-24 14:25:35 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Infoshop on 24/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
